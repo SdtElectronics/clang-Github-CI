35a977e50b92fefaf57178e17f8e648ff9cbb3a8
SdtElectronics <meiyang423@Gmail.com>
2022-06-17 00:04:15 +0800

    Example commit with tidy code
    This commit should pass the clang-tidy check.

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,6 +5,12 @@ int getRandomNumber() {
               // guaranteed to be random.
 }
 
+int nullDereference(int *p) {
+  if (p) return 0;
+  return 1;
+  // return p[0];
+}
+
 int main() {
   auto rnd = getRandomNumber();
   std::cout << rnd << std::endl;
